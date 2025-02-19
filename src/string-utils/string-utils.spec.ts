5ed1e0ec0e44ce413f67b34499477090f6c6a5a8
tom-kotlar <tomaskotlar@yahoo.co.uk>
2025-02-19 21:44:19 +0000

    Ref(string-utils): firstNonRepeatingChar(), firstRepeatingChar()

--- src/string-utils/string-utils.spec.ts
+++ src/string-utils/string-utils.spec.ts
@@ -1,5 +1,5 @@
 import { expect, jest, test, describe } from '@jest/globals';
-import { putNamesTogether, capitaliseFirstString, capitaliseString, convertToCamelCase } from "./string-utils";
+import { putNamesTogether, capitaliseFirstString, capitaliseString, convertToCamelCase, reversString, isPalindrome, charFrequency, firstNonRepeatingChar, firstRepeatingChar } from "./string-utils";
 
 
 describe('putNamesTogether', () => {
@@ -41,5 +41,70 @@ describe('convertToCamelCase', () => {
     });
 });
 
+describe('reversString', () => {
+    it('returns string if the input is empty string', () => {
+      expect(reversString('')).toEqual('');
+      expect(reversString('a')).toEqual('a');
+    });
+    it('returns message if the input is empty', () => {
+        expect(reversString(null)).toBe('Please provide valid input');
+    });
+    it('returns reversed strong if the input is string', () => {
+        expect(reversString('hello')).toBe('olleh');
+        expect(reversString('hello world')).toBe('dlrow olleh' );
+    });
+});
 
+describe('isPalindrome', () => {
+    it('returns true if the input is a palindrome', () => {
+        expect(isPalindrome('madam')).toBeTruthy();
+        expect(isPalindrome('racecar')).toBeTruthy();
+        expect(isPalindrome('A man a plan a canal Panama')).toBeTruthy();
+    });
+
+    it('returns false if the input is not a palindrome', () => {
+        expect(isPalindrome('hello')).toBeFalsy();
+        expect(isPalindrome(null)).toBeFalsy();
+        expect(isPalindrome(undefined)).toBeFalsy();
+    });
+});
+
+describe('charFrequency', () => {
+    it('returns message if the input is not valid', () => {
+        expect(charFrequency('')).toEqual({});
+        expect(charFrequency(undefined)).toEqual({"Please insert valid input": 0});
+        expect(charFrequency(null)).toEqual({"Please insert valid input": 0});
+    });
+    it('returns character count if the input is valid string', () => {
+        expect(charFrequency('hello')).toEqual({ h: 1, e: 1, l: 2, o: 1 });
+        expect(charFrequency('Hello World')).toEqual({ h: 1, e: 1, l: 3, o: 2, w: 1, r: 1, d: 1 });
+        expect(charFrequency("AaaBb")).toEqual({ a: 3, b: 2 });
+    });
+});
+
+describe('firstNonRepeatingChar', () => {
+    it('returns input if the input is a empty string', () => {
+        expect(firstNonRepeatingChar('')).toEqual(null);
+    });
+    it('returns first non repeating character if the input is a string', () => {
+        expect(firstNonRepeatingChar('aabbc')).toEqual('c');
+        expect(firstNonRepeatingChar('abcabc')).toEqual(null);
+        expect(firstNonRepeatingChar('swiss')).toEqual('w');
+        expect(firstNonRepeatingChar('a a b c c d e')).toEqual('b');
+        expect(firstNonRepeatingChar('Abraka Abraka Dabra')).toEqual('D');
+    });
+});
+
+describe('firstRepeatingChar', () => {
+    it('returns input if the input is a empty string', () => {
+        expect(firstNonRepeatingChar('')).toEqual(null);
+    });
+    it('returns firstrepeating character if the input is a string', () => {
+        expect(firstRepeatingChar('abcdefa')).toEqual('a');
+        expect(firstRepeatingChar('hello')).toEqual('l');
+        expect(firstRepeatingChar('swiss')).toEqual('s');
+        expect(firstRepeatingChar('unique')).toEqual('u');
+        expect(firstRepeatingChar('AbrakA Abraka Dabra')).toEqual('A');
+    });
+});
 
--- src/string-utils/string-utils.ts
+++ src/string-utils/string-utils.ts
@@ -1,9 +1,9 @@
-function putNamesTogether(firstName: string, lastName: string) {
+export function putNamesTogether(firstName: string, lastName: string) {
     // return the first and last names with a space between them
     return `${firstName} ${lastName}`;
 }
 
-function capitaliseFirstString(str: string) {
+export function capitaliseFirstString(str: string) {
     // return the passed string with its first letter capitalised
     if (!str) {
         return str;
@@ -11,7 +11,7 @@ function capitaliseFirstString(str: string) {
     return str[0].toUpperCase() + str.slice(1)
 }
 
-function capitaliseString(str: string) {
+export function capitaliseString(str: string) {
     // return the passed string with its first letter capitalised
     if (!str) {
         return str;
@@ -22,7 +22,7 @@ function capitaliseString(str: string) {
         .join(' '); // Join the array back into a string
 }
 
-function convertToCamelCase(words: string): string {
+export function convertToCamelCase(words: string): string {
     // convert the passed string of space-separated words to camel case
     // camel case looks like this --> camelCaseLooksLikeThis
 
@@ -40,15 +40,83 @@ function convertToCamelCase(words: string): string {
     return camelCaseWords.join('');
 }
 
+/**
+ * check if the input is valid 
+ * if yes preform a reverse
+ * @param input 
+ * @returns 
+ */
+export function reversString(input: string | null | undefined): string {
+    if (input === null || input === undefined) {
+        return 'Please provide valid input';
+    }
+    return input.split('').reverse().join('');
+}
+
+/**
+ * check if the input is valid 
+ * if yes preform a chek if the input is palindrome 
+ * @param input: string | null | undefined
+ * @returns boolean
+ */
+export function isPalindrome(input: string | null | undefined): boolean {
+    if (input === null || input === undefined) {
+        return false
+    }
+    const processed = input.replace(/\s+/g, '').toLocaleLowerCase()
+    return processed === processed.split('').reverse().join('')
+}
 
 
+export function charFrequency(input: string | null | undefined): Record<string, number> {
+    if (input === null || input === undefined) {
+        return { "Please insert valid input": 0 }
+    }
 
+    const processed = input.replace(/\s+/g, '').toLocaleLowerCase().split('')
+    const countedCharacters = Object.create(null);
+    for (const character of processed) {
+        const currCount = countedCharacters[character] ?? 0
+        countedCharacters[character] = currCount + 1;
+    }
+    return countedCharacters
+}
 
+/**
+ * we have big string which could have capitals and lowercase and spaces
+ * split the string in to individual characeter in to array so we can loop around
+ * we need store non repeating characters and their occurencies in a object
+ * loop the the object and find out if which of the value is equal of 1 and rerurn the key
+ */
+export function firstNonRepeatingChar(input: string): string | null {
+    if (input.length === 0) {
+        return null;
+    }
+    const countedCharacters: Record<string, number> = Object.create(null);
+    const processed = input.split('')
+    for (const character of processed) {
+        countedCharacters[character] = (countedCharacters[character] ?? 0) +1
+    }
 
+    for (const character of input) {
+        if (countedCharacters[character] === 1) {
+            return character
+        }
+    }
+    return null
+}
 
-export {
-  putNamesTogether,
-  capitaliseFirstString,
-  capitaliseString,
-  convertToCamelCase,
-};  
+export function firstRepeatingChar(input: string): string | null {
+    if (input.length === 0) {
+        return null;
+    }
+    const countedCharacters: Record<string, number> = Object.create(null);
+    for (const character of input) {
+        if (countedCharacters[character]) {
+            return character;
+        }
+        countedCharacters[character] = 1; 
+    }
+
+    return null;
+}
